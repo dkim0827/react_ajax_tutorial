ac7eb1974a95703bc8187f38e13008e63abe5e7d
DanielKim <dltcm0827@gmail.com>
2020-02-09 17:41:41 -0800

    added CommentList component

---
+++ src/components/CommentList/CommentList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./CommentList.css";
+
+const CommentList = () => {
+  return <ul className="CommentList"></ul>;
+};
+
+export default CommentList;
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Header from "./Header/Header";
 import PostWrapper from "./PostWrapper/PostWrapper";
 import Navigator from "./Navigator/Navigator";
 import Post from "./Post/Post";
+import CommentList from "./CommentList/CommentList";
 
-export { Header, PostWrapper, Navigator, Post };
+export { Header, PostWrapper, Navigator, Post, CommentList };
