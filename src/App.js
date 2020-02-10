5639194b570226b6957d4c2a179c36c7f6f654af
DanielKim <dltcm0827@gmail.com>
2020-02-09 17:17:49 -0800

    added PostContainer component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { PostContainer } from "./containers";
 import { Header } from "./components";
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div>
         <Header />
+        <PostContainer />
       </div>
     );
   }
