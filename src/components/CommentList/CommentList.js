e8533bcc4f3901ce42bef4fe5e58ae8ee29e4a3f
DanielKim <dltcm0827@gmail.com>
2020-02-09 18:32:22 -0800

    changed comment name to email id

--- src/components/CommentList/CommentList.js
+++ src/components/CommentList/CommentList.js
@@ -6,7 +6,11 @@ import "./CommentList.css";
 const CommentList = ({ comments }) => {
   // map data to components
   const commentList = comments.map((comment, index) => (
-    <Comment name={comment.name} body={comment.body} key={index} />
+    <Comment
+      name={comment.email.split("@")[0]}
+      body={comment.body}
+      key={index}
+    />
   ));
 
   return <ul className="CommentList">{commentList}</ul>;
